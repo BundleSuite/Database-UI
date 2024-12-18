ed00215037b071fbc84f7d7f424b2e397c2cd0fc
dev-simpleplan <95477430+dev-simpleplan@users.noreply.github.com>
2024-12-19 00:25:18 +0530

    caching fixes

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,20 @@ const nextConfig = {
     serverActions: {
       allowedOrigins: ['localhost:3000', 'https://bundlesuite-users-data.vercel.app']
     }
-  }
+  },
+  headers: async () => {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, max-age=0',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
